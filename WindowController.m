16eb8b9e49cd98ad05b374894141b1f204fb1f9b
Crazor <crazor@gmail.com>
2012-02-05 20:36:28 +0100

    Update copyright statement

--- WindowController.m
+++ WindowController.m
@@ -1,7 +1,7 @@
 /*
  * This file is part of the Tile project.
  *
- * Copyright 2009, 2010 Crazor <crazor@gmail.com>
+ * Copyright 2009-2012 Crazor <crazor@gmail.com>
  *
  * Tile is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
